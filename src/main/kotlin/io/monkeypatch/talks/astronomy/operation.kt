42bf28eaaba64f9a2f5fc25e7f77551419f574cc
Emmanuel Vinas <vinas.emmanuel@ingenico.com>
2018-04-14 17:01:08 +0200

    Added sequence use case

--- src/main/kotlin/io/monkeypatch/talks/astronomy/operation.kt
+++ src/main/kotlin/io/monkeypatch/talks/astronomy/operation.kt
@@ -35,3 +35,28 @@ fun getFirstMoonName2(): String =
         .map { it.name }
         .first()
 
+
+fun <T> timed(block: () -> T) {
+    val startTime = System.currentTimeMillis()
+    val result = block()
+    val endTime = System.currentTimeMillis()
+    val duration = endTime - startTime
+    val readable = String.format("%d min %02d seconds", duration / 60000, (duration % 60000) / 1000)
+    println("$result in $readable")
+}
+
+
+fun main(args: Array<String>) {
+    timed {
+        (0..1_000_000)
+                .asSequence()
+                .onEach {
+                    if (it % 10_000 == 0) {
+                        println("${(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1048576}MB / ${Runtime.getRuntime().totalMemory() / 1048576}MB")
+                    }
+                    println(it)
+                }
+                .map { Moon(it.toString()) }
+                .first()
+    }
+}
