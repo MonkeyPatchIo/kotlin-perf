1738aaf396124c80d2fdb8f1d4430a342ca543fc
Igor Laborie <ilaborie@gmail.com>
2018-04-15 18:19:51 +0200

    Oops !, fix tailrec

--- src/main/kotlin/io/monkeypatch/talks/factorial/tailrec-factorial.kt
+++ src/main/kotlin/io/monkeypatch/talks/factorial/tailrec-factorial.kt
@@ -3,7 +3,7 @@ package io.monkeypatch.talks.factorial
 // Note: assert(n >= 0)
 fun tailRecFactorial(n: Int): Int {
     tailrec fun aux(n: Int, acc: Int): Int =
-        if (n < 1) 1 else aux(n - 1, acc * n)
+        if (n < 1) acc else aux(n - 1, acc * n)
     return aux(n, 1)
 }
 
